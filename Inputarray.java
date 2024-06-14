d65855a99fd2043533362b9a2c41951692402a9b
Balaji <146164860+balajiananthan@users.noreply.github.com>
2024-06-14 20:41:09 +0530

    Add files via upload

---
+++ Inputarray.java
@@ -0,0 +1,11 @@
+package Arraybasic;
+
+public class Inputarray {
+public static void main(String args[]) {
+int num[]= {10,20,30,40};
+for(int i=0;i<num.length;i++) {
+	System.out.println(num[i]);
+}
+}
+}
+// print the input element
